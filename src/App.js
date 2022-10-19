aacaecdd2adf0cfb4cb184c5c7f1ebf3895daec4
JonasKaruzas <jonas.karuzas@gmail.com>
2022-10-19 22:29:50 +0300

    feat: added Form Change Handler

--- src/App.js
+++ src/App.js
@@ -8,12 +8,21 @@ import React from "react";
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.addFriend = this.addFriend.bind(this);
     this.state = {
       friends: [
         { id: 1, firstName: "John", lastName: "Smith", age: 32, city: "Kaunas" },
         { id: 2, firstName: "Maria", lastName: "Hudghes", age: 28, city: "Siauliai" },
         { id: 3, firstName: "Thomas", lastName: "Muiller", age: 33, city: "Vilnius" },
       ],
+      inputForm: {
+        id: 5,
+        firstName: null,
+        lastName: null,
+        age: null,
+        city: null,
+      },
     };
   }
 
@@ -24,12 +33,22 @@ class App extends React.Component {
     });
   }
 
+  addFriend(e) {
+    e.preventDefault();
+    console.log(this.state);
+    // this.setState((state) => console.log(...state.friends));
+  }
+
+  handleChange(e) {
+    this.setState((state) => ({ ...state, inputForm: { ...state.inputForm, [e.target.id]: e.target.value } }));
+  }
+
   render() {
     return (
       <Container>
         <Row className="d-flex justify-content-center">
           <Col className="my-3 col-12 col-sm-8">
-            <AddFriendForm />
+            <AddFriendForm data={this.state.inputForm} addHandler={this.addFriend} changeHandler={this.handleChange} />
           </Col>
         </Row>
 

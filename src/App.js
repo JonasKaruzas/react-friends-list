647f4d03143f6c2f0c6733d512c9949caaad8cb1
JonasKaruzas <jonas.karuzas@gmail.com>
2022-10-24 20:46:01 +0300

    feat: localstorage

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import { AddFriendForm } from "./AddFriendForm";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function NoFriends() {
-  return (
-    <Col fluid className="text-center">
-      Sorry... no friends in this list
-    </Col>
-  );
+  return <Col className="text-center">Sorry... no friends in this list</Col>;
 }
 
 function App() {
-  const [friends, setFriends] = useState([
-    { id: 1, firstName: "John", lastName: "Smith", age: 32, city: "Kaunas" },
-    { id: 2, firstName: "Maria", lastName: "Hudghes", age: 28, city: "Siauliai" },
-    { id: 3, firstName: "Thomas", lastName: "Muiller", age: 33, city: "Vilnius" },
-  ]);
+  const [friends, setFriends] = useState(() => {
+    const savedFriends = localStorage.getItem("FriendsList");
+    const initialValue = JSON.parse(savedFriends);
+    return initialValue || "";
+  });
+
+  useEffect(() => {
+    localStorage.setItem("FriendsList", JSON.stringify(friends));
+  }, [friends]);
 
   function deleteHandler(id) {
     setFriends(friends.filter((friend) => friend.id !== id));
